9fa26c6d71ef78bee3b64109604b983a64c06c3f
Coursera <aide.abdel@gmail.com>
2024-02-22 16:32:19 -0500

    App running with Code First EF-Core

---
+++ P2/RecipeFinder/Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeFinder.Model;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
---
+++ P2/RecipeFinder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RecipeFinder.Model;
+
+namespace RecipeFinder.APP
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Recipe Finder starting...");
+
+            var repo = new Repository();
+            Console.WriteLine(repo.ListAllUsers());
+            repo.AddNewUser();
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine(repo.ListAllUsers());
+        }
+    }
+}
